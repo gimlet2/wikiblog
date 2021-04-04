04795363f8f520b7a48a7b12262203694c9798b8
gimlet2 <andrey.chernishov@gmail.com>
2021-04-04 23:12:31 +0200

    add tags

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -68,6 +68,20 @@ module.exports = {
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
     ['@vuepress/blog',{
+      frontmatters: [
+        {
+          // Unique ID of current classification
+          id: 'tag',
+          // Decide that the frontmatter keys will be grouped under this classification
+          keys: ['tag','tags'],
+          // Path of the `entry page` (or `list page`)
+          path: '/tag/',
+          // Layout of the `entry page`
+          layout: 'Tags',
+          // Layout of the `scope page`
+          scopeLayout: 'Tag'
+        },
+      ],
       directories: [
         {
           // Unique ID of current classification
